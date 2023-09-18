c9cdad66144de0e1e17e31c81dc2451666c4c2af
Mario Teves <mariotevesvn@gmail.com>
2023-09-18 23:23:41 +0700

    Move Exercise_03 to Session_05

---
+++ src/Session_05/Exercise_03.java
@@ -0,0 +1,33 @@
+package Session_05;
+
+public class Exercise_03 {
+	public static void main(String[]args) {
+		int num1 = 150;
+		int num2 = 8;
+		char mathOperator = '/';
+		
+		switch (mathOperator) {
+		case '+':
+			System.out.println("Addition");
+			System.out.println(num1 + " plus " + num2 + " is equal to " + (num1 + num2));
+			break;
+		case '-':
+			System.out.println("Subtraction");
+			System.out.println(num1 + " minus " + num2 + " is equal to " + (num1 - num2));
+
+			break;
+		case '*':
+			System.out.println("Multiplication");
+			System.out.println(num1 + " times " + num2 + " is equal to " + (num1 * num2));
+			break;
+		case '/':
+			System.out.println("Division");
+			System.out.println(num1 + " divided by " + num2 + " is equal to " + (num1 / num2));
+
+			break;
+			default:
+				System.out.println("Sorry,invalid operator.Please try again...");
+		}
+	}
+
+}
