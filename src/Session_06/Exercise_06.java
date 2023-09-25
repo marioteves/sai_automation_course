5ce2624831668b552014b49243b7a2beb30c8a73
Mario Teves <mariotevesvn@gmail.com>
2023-09-25 18:02:43 +0700

    Updated Exercise_06

--- src/Session_06/Exercise_06.java
+++ src/Session_06/Exercise_06.java
@@ -4,7 +4,7 @@ public class Exercise_06 {
 	public static void main(String[] args) {
 		int[] myNum = {3,4,6,7,2,3};
 		for(int i = (myNum.length - 1); i < myNum.length; i--) {
-			System.out.print(myNum[i]);
+			System.out.print(myNum[i] + ",");
 			if(i < 1) {
 				break;
 			}
--- src/Session_06/Exercise_07.java
+++ src/Session_06/Exercise_07.java
@@ -1,5 +1,8 @@
 package Session_06;
 
 public class Exercise_07 {
+	public static void main(String[] args) {
+		
+	}
 
 }
