38a977942da14b2610abe507bd14449544c3f6af
Mario Teves <mariotevesvn@gmail.com>
2023-09-25 17:46:53 +0700

    Updated Exercise_06

--- src/Session_06/Exercise_06.java
+++ src/Session_06/Exercise_06.java
@@ -1,5 +1,14 @@
 package Session_06;
 
 public class Exercise_06 {
+	public static void main(String[] args) {
+		int[] myNum = {3,4,6,7,2,3};
+		for(int i = (myNum.length - 1); i < myNum.length; i--) {
+			System.out.print(myNum[i]);
+			if(i < 1) {
+				break;
+			}
+		}
+	}
 
 }
