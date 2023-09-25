78834982807badcd50ad3c5eb1dbc7c7f0a5481e
Mario Teves <mariotevesvn@gmail.com>
2023-09-25 18:53:18 +0700

    Add comments to Exercise_07

--- src/Session_06/Exercise_07.java
+++ src/Session_06/Exercise_07.java
@@ -5,16 +5,19 @@ public class Exercise_07 {
 		String text1 = "x12Fgfa";
 		String text2 = "password";
 		
+		//Exercise_07.1
 		System.out.println("Exercise_07.1");
 		System.out.println(text2 + ": ".concat(text1));
 		
 		System.out.println("\n");
 		
+		//Exercise_07.2
 		System.out.println("Exercise_07.2");
 		System.out.println(text1 + " ".concat(text2));
 		
 		System.out.println("\n");
 		
+		//Exercise_07.3
 		System.out.println("Exercise_07.3");
 		String a ="Automation";
 		String b ="Auto";
