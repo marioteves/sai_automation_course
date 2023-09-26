80c64c4ac8b8e21e07f2de672fb63658dd5a8f68
Mario Teves <mariotevesvn@gmail.com>
2023-09-26 12:33:51 +0700

    Updated Exercise_6 and Exercise_07

--- src/Session_06/Exercise_07.java
+++ src/Session_06/Exercise_07.java
@@ -2,34 +2,33 @@ package Session_06;
 
 public class Exercise_07 {
 	public static void main(String[] args) {
-		String text1 = "x12Fgfa";
-		String text2 = "password";
-		
-		//Exercise_07.1
+		String text1 = "password: x12Fgfa";
+		String text2 = "x12Fgfa password";
+
+		// Exercise_07.1
 		System.out.println("Exercise_07.1");
-		System.out.println(text2 + ": ".concat(text1));
-		
-		System.out.println("\n");
-		
-		//Exercise_07.2
+		System.out.println(text1.substring(10));
+
+		System.out.println("\n-----------------------");
+
+		// Exercise_07.2
 		System.out.println("Exercise_07.2");
-		System.out.println(text1 + " ".concat(text2));
-		
-		System.out.println("\n");
-		
-		//Exercise_07.3
+		System.out.println(text2.substring(0,7));
+
+		System.out.println("\n-----------------------");
+
+		// Exercise_07.3
 		System.out.println("Exercise_07.3");
-		String a ="Automation";
-		String b ="Auto";
-		
-		if(a==b) {
+		String a = "Automation";
+		String b = "Auto";
+
+		if (a.equals(b)) {
 			System.out.println("a equal b");
-		}else if (a.contains(b)) {
-				System.out.println("a contains b");
-			} else if(a != b){
-				System.out.println("a not equal b");
-			}
+		} else if (a.contains(b)) {
+			System.out.println("a contains b");
+		} else {
+			System.out.println("a not equal b");
 		}
-	
+	}
 
 }
